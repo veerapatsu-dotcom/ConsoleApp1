5d00ce34f1f46add53d36b0d4809334599246575
veerapatsu-dotcom <veerapat.su@kkumail.com>
2025-12-02 11:21:29 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace veerapat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // print to the console
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Veerapat Sukpilaikul");
+            Console.WriteLine("Student ID: 6834501939");
+            Console.WriteLine("Major : Computer Information Science");
+            Console.WriteLine("Faculty : IS");
+            Console.WriteLine("khon kaen university");
+        }
+    }
+}
